e43891c2cbe9f283713af8396b2d53a843e6ee7a
sami157 <sami.eee157@gmail.com>
2026-01-29 19:00:59 +0600

    better-auth basic setup

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import express, { type Application } from "express";
 
 const app:Application = express();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
     res.send("Hello, this is Skill Bridge server!");
 })
